0e1a35d03a59079488ffb9a3b53ea4643c061ffd
Sol Survivor <37097099+lordtaltosh@user.noreply.github.com>
2018-07-24 15:20:45 -0500

    First upload

---
+++ keys.js
@@ -0,0 +1,11 @@
+exports.twitterKeys = {
+    consumer_key: 'PQQxfrQOHIn4ArYcCes0fqe8J',
+    consumer_secret: 'JCX0AinYjWXupznfTcDHtRFEpTnpcjnbEVJsLBF25cnEt8RRbv',
+    access_token_key: '95842629-0c09Zjoc5zts0l2DgUgLA5HJOkotp1A5J4iIdFXga',
+    access_token_secret: 'goPP3u8Wx8D1dZfC1bYZu2fOAU2jfrWCNiaGikOk5j1Rs',
+}
+
+exports.spotifyKeys = {
+    id: 'a146aeaca5424c61a4be5c97cce0b02a',
+    secret: '5d7cb59a13b2473f85d85eb7ad5851ce',
+}
